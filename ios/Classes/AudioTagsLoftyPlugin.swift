4f2b7ff1d0d724da4d884d08f41956204c4020e9
AfalpHy <736353503@qq.com>
2026-02-12 13:57:45 +0800

    fix: avoid dead-code elimination

--- ios/Classes/AudioTagsLoftyPlugin.swift
+++ ios/Classes/AudioTagsLoftyPlugin.swift
@@ -1,7 +1,12 @@
 import Flutter
+import UIKit
+
+@_silgen_name("lofty_read_picture")
+func lofty_read_picture_raw(_ path: UnsafePointer<Int8>?) -> UnsafeMutableRawPointer?
 
 public class AudioTagsLoftyPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    // Intentionally empty
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        // avoid dead code elimination
+        _ = lofty_read_picture_raw(nil)
+    }
 }
