abad654e6330044b9421d5e80e73ef4737954c03
AfalpHy <736353503@qq.com>
2026-02-12 15:19:08 +0800

    fix: keep all symbols

--- ios/Classes/AudioTagsLoftyPlugin.swift
+++ ios/Classes/AudioTagsLoftyPlugin.swift
@@ -1,12 +1,23 @@
 import Flutter
 import UIKit
 
+@_silgen_name("lofty_read_metadata")
+func lofty_read_metadata_raw(_ path: UnsafePointer<Int8>?) -> UnsafeMutableRawPointer?
+
 @_silgen_name("lofty_read_picture")
 func lofty_read_picture_raw(_ path: UnsafePointer<Int8>?) -> UnsafeMutableRawPointer?
 
+@_silgen_name("lofty_free_metadata")
+func lofty_free_metadata_raw(_ ptr: UnsafeMutableRawPointer?)
+
+@_silgen_name("lofty_free_picture")
+func lofty_free_picture_raw(_ ptr: UnsafeMutableRawPointer?)
+
 public class AudioTagsLoftyPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
-        // avoid dead code elimination
+        _ = lofty_read_metadata_raw(nil)
         _ = lofty_read_picture_raw(nil)
+        lofty_free_metadata_raw(nil)
+        lofty_free_picture_raw(nil)
     }
 }
